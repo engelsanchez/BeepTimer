90ee5e818fd53b6c0feaa86eed6f9f7d1e59a967
Engel A. Sanchez <engel.sanchez@gmail.com>
2013-01-05 17:28:06 -0500

    Basically works
    There is a "big" beep that means 5 minutes and a small beep that means 1
    minute. Every minute, a sequence of big beeps followed by small beeps
    tells the elapsed time this way. It is working even in the background
    with the screen locked. Yay!

--- BeepTimer/AppDelegate.m
+++ BeepTimer/AppDelegate.m
@@ -34,12 +34,21 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [[AVAudioSession sharedInstance] setActive: YES error: NULL];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+// When Audio session is interrupted.
+- (void) beginInterruption {
+    
+}
+
+- (void) endInterruptionWithFlags: (NSUInteger) flags {
+    
 }
 
 @end
--- BeepTimer/ViewController.m
+++ BeepTimer/ViewController.m
@@ -8,12 +8,19 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    NSArray * playSequence;
+    int playIndex;
+}
 
 @property (strong, nonatomic) IBOutlet UILabel * label;
 @property (strong, nonatomic) IBOutlet UIButton * button;
 @property (strong, nonatomic) NSTimer * timer;
+@property (strong, nonatomic) AVAudioPlayer * player1;
+@property (strong, nonatomic) AVAudioPlayer * player2;
 @property int counter;
+@property int period;
+@property UIBackgroundTaskIdentifier bgTask;
 
 @end
 
@@ -22,6 +29,9 @@
 
 @synthesize label;
 @synthesize button;
+@synthesize player1;
+@synthesize player2;
+@synthesize bgTask;
 
 - (void) buttonDown: (id) sender event: (UIEvent*) e {
     if (self.timer) {
@@ -35,13 +45,62 @@
     }
 }
 
-- (void) onTimerTick:data {
+- (AVAudioPlayer *) preparePlayer: (NSString*) name {
+    NSURL * url = [[NSBundle mainBundle] URLForResource:name withExtension:@"mp3"];
+    NSError * err = nil;
+    AVAudioPlayer * newPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&err];
+    [newPlayer setDelegate: self];
+    [newPlayer prepareToPlay];
+    return newPlayer;
+}
+
+- (void) playSound {
+    [self.player1 setCurrentTime:0];
+    [self.player1 play];
+}
+
+- (void) preparePlaySequence {
+    const int maxBig = 6;
+    int mins = self.counter / _period;
+    int numSmall = mins % 5;
+    int numBig = (mins / 5) % maxBig;
+    NSLog(@"Will prepare play sequence with %d bigs and %d smalls", numBig, numSmall);
+    NSMutableArray * arr = [NSMutableArray arrayWithCapacity: numBig + numSmall];
+    for (int i=0;i<numBig; i++)
+        [arr addObject: player1];
+    for (int i=0;i<numSmall; i++)
+        [arr addObject: player2];
+    playSequence = arr;
+    playIndex = 0;
+    NSLog(@"Prepared play sequence with %d bigs and %d smalls", numBig, numSmall);
+    [[AVAudioSession sharedInstance] setActive: YES error: NULL];
+}
+
+- (BOOL) playNext {
+    int count = [playSequence count];
+    if (playSequence && playIndex < count) {
+        AVAudioPlayer * player = [playSequence objectAtIndex: playIndex++];
+        [player setCurrentTime:0];
+        [player play];
+        return YES;
+    } else
+        return NO;
+}
+
+- (void) onTimerTick: data {
     self.counter++;
     [self refreshLabel];
+    if ((self.counter % _period) == 0) {
+        NSLog(@"Will prepare play sequence");
+        [self preparePlaySequence];
+        [self playNext];
+    }
+    //UIApplication * app = [UIApplication sharedApplication];
+    //NSLog(@"Time left is %f", app.backgroundTimeRemaining);
 }
 
 - (void) refreshLabel {
-    int mins = self.counter / 60;
+    int mins = (self.counter / 60) % 60; // wrap around at 60 mins
     int secs = self.counter % 60;
     label.text = [NSString stringWithFormat:@"%02d:%02d", mins, secs];
 }
@@ -65,14 +124,47 @@
 
 - (void)viewDidLoad
 {
+    _period = 60;
     [super viewDidLoad];
+    playSequence = nil;
+    playIndex = 0;
     [self.button addTarget:self action:@selector(buttonDown:event:) forControlEvents:UIControlEventTouchUpInside];
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error: NULL];
+    UIApplication  *app = [UIApplication sharedApplication];
+    [app beginReceivingRemoteControlEvents];
+    bgTask = [app beginBackgroundTaskWithExpirationHandler:^{
+        [app endBackgroundTask:bgTask];
+        bgTask = UIBackgroundTaskInvalid;
+    }];
+    self.player1 = [self preparePlayer: @"Big"];
+    self.player2 = [self preparePlayer: @"Little"];
 }
 
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
+}
+
+// When Audio session is interrupted.
+- (void) beginInterruption {
+    
+}
+
+- (void) endInterruptionWithFlags: (NSUInteger) flags {
+    
+}
+
+- (void) audioPlayerDidFinishPlaying: (AVAudioPlayer*) player successfully: (BOOL) flag {
+    if (![self playNext]) {
+        UIApplication  *app = [UIApplication sharedApplication];
+        [app endBackgroundTask:bgTask];
+        bgTask = [app beginBackgroundTaskWithExpirationHandler: ^{
+            [app endBackgroundTask:bgTask];
+            bgTask = UIBackgroundTaskInvalid;
+        }];
+    }
+    
 }
 
 @end
